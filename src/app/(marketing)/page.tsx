ccd7706a4d6e578e6550fb55cc332735c5171c10
rama07091996 <ramakrishna.anandrk@gmail.com>
2026-09-08 13:54:35 +0530

    feat: remove video showcase from website landing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Hero } from '@/components/landing/Hero';
-import { AIVideoShowcase } from '@/components/landing/AIVideoShowcase';
 import { FeatureGrid } from '@/components/landing/FeatureGrid';
 import { InteractiveCalculator } from '@/components/landing/InteractiveCalculator';
 import { Pricing } from '@/components/landing/Pricing';
@@ -10,7 +9,6 @@ export default function LandingPage() {
   return (
     <div className="w-full">
       <Hero />
-      <AIVideoShowcase />
       <FeatureGrid />
       <InteractiveCalculator />
       <Pricing />
